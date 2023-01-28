ad0df5c89e6d5249aec94a7b01ef28ac5e7321cd
romeerez <romadzao@gmail.com>
2023-01-27 21:58:36 -0500

    Delete compiled file

--- packages/esm/src/main.js
+++
@@ -1,6 +0,0 @@
-import { orchidORM } from 'orchid-orm';
-import { createDb } from 'pqb';
-import { rakeDb } from 'rake-db';
-import { instanceToZod } from 'orchid-orm-schema-to-zod';
-import { createFactory } from 'orchid-orm-test-factory';
-console.log(orchidORM, createDb, rakeDb, instanceToZod, createFactory);
